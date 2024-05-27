dde71b35f9e77cdae69288ea9570d1602b6bc7f0
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2024-05-26 22:16:22 -0500

    Principio del manejo de sesión

--- frontend/src/HomeProductos/ShoppingCartNavbar.js
+++ frontend/src/HomeProductos/ShoppingCartNavbar.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './ShoppingCartNavbar.css';
 import cesta from '../img/cestados.png';
+import userImage from '../img/usuario (1).png'
 
 const ShoppingCartNavbar = ({ countProducts }) => {
+
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleIconClick = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
     <header className="shopping-cart-header">
       <div className="shopping-cart-container">
         <h1 className="shopping-cart-title">Shopping Cart</h1>
+        <div className="user-icon-container">
+          <div className="user-icon-circle" onClick={handleIconClick}>
+          <img src={userImage} alt="User Icon" className="user-image" />
+            <i className="fas fa-user"></i>
+          </div>
+          {showInfo && (
+            <div className="user-info-box">
+              <p className="user-name">Nombre Completo</p>
+              <p className="user-email">Correo</p>
+              <button className="logout-button" >
+                Cerrar Sesión
+              </button>
+            </div>
+          )}
+        </div>
         <div className="shopping-cart-icon-container">
           <Link to="/cart" className="shopping-cart-link">
             <img id="carrito" className="carrito" src={cesta} alt="Carrito" />
