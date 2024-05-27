9395907b975736404f8c5e724e206ed654972dc3
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2024-05-26 23:49:06 -0500

    Usuario en LocalStorage

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,51 @@
 const express = require("express");
-const mysql = require('mysql');
-const cors = require('cors');
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "electrostar"
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "electrostar",
+});
 
-app.post('/signup', (req, res) => {
-    const sql = "INSERT INTO users (`documento`,`nombre`,`primerApellido`,`segundoApellido`,`email`,`password`) VALUES (?)";
-    const values = [
-        req.body.documento,
-        req.body.nombre,
-        req.body.primerApellido,
-        req.body.segundoApellido,
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [values], (err, data) => {
-        if(err) {
-            return res.json("Error");
-        }
-        return res.json(data);
-    })
-})
+app.post("/signup", (req, res) => {
+  const sql =
+    "INSERT INTO users (`documento`,`nombre`,`primerApellido`,`segundoApellido`,`email`,`password`) VALUES (?)";
+  const values = [
+    req.body.documento,
+    req.body.nombre,
+    req.body.primerApellido,
+    req.body.segundoApellido,
+    req.body.email,
+    req.body.password,
+  ];
+  db.query(sql, [values], (err, data) => {
+    if (err) {
+      return res.json("Error");
+    }
+    return res.json(data);
+  });
+});
 
-app.post('/signup', (req, res) => {
-    const sql = "SELECT * FROM users WHERE `email` = ? AND `password` = ?";
-    db.query(sql, [req.body.email,req.body.password], (err, data) => {
-        if(err) {
-            return res.json("Error");
-        }
-        if(data.length > 0){
-            return res.json("Success");
-        }else{
-            return res.json("Failed")
-        }
-    })
-})
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM users WHERE `email` = ? AND `password` = ?";
+  db.query(sql, [req.body.email, req.body.password], (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    if (data.length > 0) {
+      return res.json("Success");
+    } else {
+      return res.json("Failed");
+    }
+  });
+});
 
 app.listen(8081, () => {
-    console.log("listening");
-})
+  console.log("listening");
+});
