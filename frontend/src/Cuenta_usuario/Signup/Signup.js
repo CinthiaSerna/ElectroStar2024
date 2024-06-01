e51c4582d7f862acb6c9ad6e70c3b63ec2a5c238
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2024-05-31 23:16:59 -0500

    soluciones

--- frontend/src/Cuenta_usuario/Signup/Signup.js
+++ frontend/src/Cuenta_usuario/Signup/Signup.js
@@ -75,285 +75,156 @@ function Signup() {
     if (Object.keys(loginErrors).length === 0) {
       const { email, password } = values;
 
+      // Handle admin login
       if (email === "admin@example.com" && password === "admin123") {
         localStorage.setItem('user', JSON.stringify({ email: "admin@example.com", password: "admin123" }));
         navigate("/admin");
+      } else if (email === "pepito@gmail.com" && password === "Pepito12345") {
+        localStorage.setItem('user', JSON.stringify({ email: "pepito@gmail.com", password: "Pepito12345" }));
+        navigate("/shopping");
       } else {
-        // Check for "usuario quemado"
-        if (email === "pepito@gmail.com" && password === "Pepito12345") {
-          localStorage.setItem('user', JSON.stringify({ email: "pepito@gmail.com", password: "Pepito12345" }));
-          navigate("/shopping");  // Replace with the URL you want for "usuario quemado"
-        } else {
-          // Validate user credentials against backend
-          axios
-            .post(`https://electrostar2024-production.up.railway.app/login`, {
-              email: email,
-              password: password,
-            })
-            .then((res) => {
-              console.log("res", res);
-              const status = res.data;
-              if (status === "Success") {
-                localStorage.setItem(
-                  "user",
-                  JSON.stringify({
-                    email: email,
-                    password: password,
-                  })
-                );
-                navigate("/shopping");
-              } else if (status === "Failed") {
-                console.error("Login failed:");
-                setErrors({ login: "Credenciales inválidas" });
-                alert("Credenciales inválidas");
-              }
-            });
-        }
+        axios
+          .post(`electrostar2024-production.up.railway.app/login`, {
+            email: email,
+            password: password,
+          })
+          .then((res) => {
+            console.log("res", res);
+            const status = res.data;
+            status === "Success" &&
+              localStorage.setItem(
+                "user",
+                JSON.stringify({
+                  email: email,
+                  password: password,
+                })
+              );
+            status === "Success" && navigate("/shopping");
+            if (status === "Failed") {
+              console.error("Login failed:");
+              setErrors({ login: "Credenciales inválidas" });
+              alert("Credenciales inválidas");
+            }
+          });
       }
-
-      // // Handle admin login
-      // if (email === "admin@example.com" && password === "admin123") {
-      //   localStorage.setItem('user', JSON.stringify({ email: "admin@example.com", password: "admin123" }));
-      //   navigate("/admin");
-      // } else {
-      //   // Validate user credentials against backend
-      //   axios
-      //     .post(`electrostar2024-production.up.railway.app/login`, {
-      //       email: email,
-      //       password: password,
-      //     })
-      //     .then((res) => {
-      //       console.log("res", res);
-      //       const status = res.data;
-      //       status === "Success" &&
-      //         localStorage.setItem(
-      //           "user",
-      //           JSON.stringify({
-      //             email: email,
-      //             password: password,
-      //           })
-      //         );
-      //       status === "Success" && navigate("/shopping");
-      //       if (status === "Failed") {
-      //         console.error("Login failed:");
-      //         setErrors({ login: "Credenciales inválidas" });
-      //         alert("Credenciales inválidas");
-      //       }
-      //     });
     }
-  }
-};
-
-const iniciarSesion = () => {
-  if (window.innerWidth > 850) {
-    setFormularioLoginVisible(true);
-    setContenedorLeft("10px");
-    setFormularioRegisterVisible(false);
-    setCajaTraseraRegisterOpacity(1);
-    setCajaTraseraLoginOpacity(0);
-  } else {
-    setFormularioLoginVisible(true);
-    setContenedorLeft("0px");
-    setFormularioRegisterVisible(false);
-    setCajaTraseraRegisterVisible(true);
-    setCajaTraseraLoginVisible(false);
-  }
-};
-
-const register = () => {
-  if (window.innerWidth > 850) {
-    setFormularioRegisterVisible(true);
-    setContenedorLeft("410px");
-    setFormularioLoginVisible(false);
-    setCajaTraseraRegisterOpacity(0);
-    setCajaTraseraLoginOpacity(1);
-  } else {
-    setFormularioRegisterVisible(true);
-    setContenedorLeft("0px");
-    setFormularioLoginVisible(false);
-    setCajaTraseraRegisterVisible(false);
-    setCajaTraseraLoginVisible(true);
-  }
-};
-
-const anchoPage = () => {
-  if (window.innerWidth > 850) {
-    setCajaTraseraRegisterVisible(true);
-    setCajaTraseraLoginVisible(true);
-  } else {
-    setCajaTraseraRegisterVisible(true);
-    setCajaTraseraRegisterOpacity(1);
-    setCajaTraseraLoginVisible(false);
-    setFormularioLoginVisible(true);
-    setContenedorLeft("0px");
-    setFormularioRegisterVisible(false);
-  }
-};
-
-useEffect(() => {
-  anchoPage();
-  window.addEventListener("resize", anchoPage);
-  return () => {
-    window.removeEventListener("resize", anchoPage);
   };
-}, []);
 
-return (
-  <main className="main-container">
-    <div className="contenedor__todo">
-      <div
-        className="caja__trasera"
-        style={{
-          display:
-            cajaTraseraRegisterVisible || cajaTraseraLoginVisible
-              ? "flex"
-              : "none",
-        }}
-      >
+  const iniciarSesion = () => {
+    if (window.innerWidth > 850) {
+      setFormularioLoginVisible(true);
+      setContenedorLeft("10px");
+      setFormularioRegisterVisible(false);
+      setCajaTraseraRegisterOpacity(1);
+      setCajaTraseraLoginOpacity(0);
+    } else {
+      setFormularioLoginVisible(true);
+      setContenedorLeft("0px");
+      setFormularioRegisterVisible(false);
+      setCajaTraseraRegisterVisible(true);
+      setCajaTraseraLoginVisible(false);
+    }
+  };
+
+  const register = () => {
+    if (window.innerWidth > 850) {
+      setFormularioRegisterVisible(true);
+      setContenedorLeft("410px");
+      setFormularioLoginVisible(false);
+      setCajaTraseraRegisterOpacity(0);
+      setCajaTraseraLoginOpacity(1);
+    } else {
+      setFormularioRegisterVisible(true);
+      setContenedorLeft("0px");
+      setFormularioLoginVisible(false);
+      setCajaTraseraRegisterVisible(false);
+      setCajaTraseraLoginVisible(true);
+    }
+  };
+
+  const anchoPage = () => {
+    if (window.innerWidth > 850) {
+      setCajaTraseraRegisterVisible(true);
+      setCajaTraseraLoginVisible(true);
+    } else {
+      setCajaTraseraRegisterVisible(true);
+      setCajaTraseraRegisterOpacity(1);
+      setCajaTraseraLoginVisible(false);
+      setFormularioLoginVisible(true);
+      setContenedorLeft("0px");
+      setFormularioRegisterVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    anchoPage();
+    window.addEventListener("resize", anchoPage);
+    return () => {
+      window.removeEventListener("resize", anchoPage);
+    };
+  }, []);
+
+  return (
+    <main className="main-container">
+      <div className="contenedor__todo">
         <div
-          className="caja__trasera-login"
+          className="caja__trasera"
           style={{
-            display: cajaTraseraLoginVisible ? "block" : "none",
-            opacity: cajaTraseraLoginOpacity,
+            display:
+              cajaTraseraRegisterVisible || cajaTraseraLoginVisible
+                ? "flex"
+                : "none",
           }}
         >
-          <h3>¿Ya tienes una cuenta?</h3>
-          <p>Inicia sesión para entrar en la página</p>
-          <button onClick={iniciarSesion} id="btn__iniciar-sesion">
-            Iniciar Sesión
-          </button>
+          <div
+            className="caja__trasera-login"
+            style={{
+              display: cajaTraseraLoginVisible ? "block" : "none",
+              opacity: cajaTraseraLoginOpacity,
+            }}
+          >
+            <h3>¿Ya tienes una cuenta?</h3>
+            <p>Inicia sesión para entrar en la página</p>
+            <button onClick={iniciarSesion} id="btn__iniciar-sesion">
+              Iniciar Sesión
+            </button>
+          </div>
+          <div
+            className="caja__trasera-register"
+            style={{
+              display: cajaTraseraRegisterVisible ? "block" : "none",
+              opacity: cajaTraseraRegisterOpacity,
+            }}
+          >
+            <h3>¿Aún no tienes una cuenta?</h3>
+            <p>Regístrate para que puedas iniciar sesión</p>
+            <button onClick={register} id="btn__registrarse">
+              Registrarse
+            </button>
+          </div>
         </div>
+
         <div
-          className="caja__trasera-register"
-          style={{
-            display: cajaTraseraRegisterVisible ? "block" : "none",
-            opacity: cajaTraseraRegisterOpacity,
-          }}
+          className="contenedor__login-register"
+          style={{ left: contenedorLeft }}
         >
-          <h3>¿Aún no tienes una cuenta?</h3>
-          <p>Regístrate para que puedas iniciar sesión</p>
-          <button onClick={register} id="btn__registrarse">
-            Registrarse
-          </button>
-        </div>
-      </div>
-
-      <div
-        className="contenedor__login-register"
-        style={{ left: contenedorLeft }}
-      >
-        <form
-          className="formulario__login"
-          style={{ display: formularioLoginVisible ? "block" : "none" }}
-          onSubmit={handleLogin} //handleLogin
-        >
-          <h2>Iniciar Sesión</h2>
-          <input
-            type="text"
-            placeholder="Correo Electrónico *"
-            name="email"
-            value={values.email}
-            onChange={handleInput}
-            className="controls"
-          />
-          {errors.email && (
-            <span className="text-danger">{errors.email}</span>
-          )}
-          <input
-            type="password"
-            placeholder="Contraseña *"
-            name="password"
-            value={values.password}
-            onChange={handleInput}
-            className="controls"
-          />
-          {errors.password && (
-            <span className="text-danger">{errors.password}</span>
-          )}
-          {errors.login && (
-            <span className="text-danger">{errors.login}</span>
-          )}
-          <button type="submit" className="login-button">
-            Entrar
-          </button>
-        </form>
-
-        <form
-          className="formulario__register"
-          style={{ display: formularioRegisterVisible ? "block" : "none" }}
-          onSubmit={handleSubmit}
-        >
-          <h2>Registrarse</h2>
-          <div className="form-row">
+          <form
+            className="formulario__login"
+            style={{ display: formularioLoginVisible ? "block" : "none" }}
+            onSubmit={handleLogin} //handleLogin
+          >
+            <h2>Iniciar Sesión</h2>
             <input
               type="text"
-              placeholder="Documento *"
-              name="documento"
-              value={values.documento}
-              onChange={handleInput}
-              className="controls"
-            />
-            <input
-              type="text"
-              placeholder="Nombre *"
-              name="nombre"
-              value={values.nombre}
-              onChange={handleInput}
-              className="controls"
-            />
-          </div>
-          {errors.documento && (
-            <span className="text-danger">{errors.documento}</span>
-          )}
-          {errors.nombre && (
-            <span className="text-danger">{errors.nombre}</span>
-          )}
-          <div className="form-row">
-            <input
-              type="text"
-              placeholder="Primer Apellido *"
-              name="primerApellido"
-              value={values.primerApellido}
-              onChange={handleInput}
-              className="controls"
-            />
-            <input
-              type="text"
-              placeholder="Segundo Apellido (opcional)"
-              name="segundoApellido"
-              value={values.segundoApellido}
-              onChange={handleInput}
-              className="controls"
-            />
-          </div>
-          {errors.primerApellido && (
-            <span className="text-danger">{errors.primerApellido}</span>
-          )}
-          <div className="form-row">
-            <input
-              type="text"
-              placeholder="Correo *"
+              placeholder="Correo Electrónico *"
               name="email"
               value={values.email}
               onChange={handleInput}
               className="controls"
             />
-            <input
-              type="text"
-              placeholder="Confirmar Correo *"
-              name="confirmarEmail"
-              value={values.confirmarEmail}
-              onChange={handleInput}
-              className="controls"
-            />
-          </div>
-          {errors.email && (
-            <span className="text-danger">{errors.email}</span>
-          )}
-          {errors.confirmarEmail && (
-            <span className="text-danger">{errors.confirmarEmail}</span>
-          )}
-          <div className="form-row">
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
             <input
               type="password"
               placeholder="Contraseña *"
@@ -362,29 +233,124 @@ return (
               onChange={handleInput}
               className="controls"
             />
-            <input
-              type="password"
-              placeholder="Confirmar Contraseña *"
-              name="confirmarPassword"
-              value={values.confirmarPassword}
-              onChange={handleInput}
-              className="controls"
-            />
-          </div>
-          {errors.password && (
-            <span className="text-danger">{errors.password}</span>
-          )}
-          {errors.confirmarPassword && (
-            <span className="text-danger">{errors.confirmarPassword}</span>
-          )}
-          <button type="submit" className="login-button">
-            Registrarme
-          </button>
-        </form>
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+            {errors.login && (
+              <span className="text-danger">{errors.login}</span>
+            )}
+            <button type="submit" className="login-button">
+              Entrar
+            </button>
+          </form>
+
+          <form
+            className="formulario__register"
+            style={{ display: formularioRegisterVisible ? "block" : "none" }}
+            onSubmit={handleSubmit}
+          >
+            <h2>Registrarse</h2>
+            <div className="form-row">
+              <input
+                type="text"
+                placeholder="Documento *"
+                name="documento"
+                value={values.documento}
+                onChange={handleInput}
+                className="controls"
+              />
+              <input
+                type="text"
+                placeholder="Nombre *"
+                name="nombre"
+                value={values.nombre}
+                onChange={handleInput}
+                className="controls"
+              />
+            </div>
+            {errors.documento && (
+              <span className="text-danger">{errors.documento}</span>
+            )}
+            {errors.nombre && (
+              <span className="text-danger">{errors.nombre}</span>
+            )}
+            <div className="form-row">
+              <input
+                type="text"
+                placeholder="Primer Apellido *"
+                name="primerApellido"
+                value={values.primerApellido}
+                onChange={handleInput}
+                className="controls"
+              />
+              <input
+                type="text"
+                placeholder="Segundo Apellido (opcional)"
+                name="segundoApellido"
+                value={values.segundoApellido}
+                onChange={handleInput}
+                className="controls"
+              />
+            </div>
+            {errors.primerApellido && (
+              <span className="text-danger">{errors.primerApellido}</span>
+            )}
+            <div className="form-row">
+              <input
+                type="text"
+                placeholder="Correo *"
+                name="email"
+                value={values.email}
+                onChange={handleInput}
+                className="controls"
+              />
+              <input
+                type="text"
+                placeholder="Confirmar Correo *"
+                name="confirmarEmail"
+                value={values.confirmarEmail}
+                onChange={handleInput}
+                className="controls"
+              />
+            </div>
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
+            {errors.confirmarEmail && (
+              <span className="text-danger">{errors.confirmarEmail}</span>
+            )}
+            <div className="form-row">
+              <input
+                type="password"
+                placeholder="Contraseña *"
+                name="password"
+                value={values.password}
+                onChange={handleInput}
+                className="controls"
+              />
+              <input
+                type="password"
+                placeholder="Confirmar Contraseña *"
+                name="confirmarPassword"
+                value={values.confirmarPassword}
+                onChange={handleInput}
+                className="controls"
+              />
+            </div>
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+            {errors.confirmarPassword && (
+              <span className="text-danger">{errors.confirmarPassword}</span>
+            )}
+            <button type="submit" className="login-button">
+              Registrarme
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
 }
 
 export default Signup;
