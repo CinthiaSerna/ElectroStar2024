2111ff9958bb8bd7cc4df6f34741bff4856165fd
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2024-05-31 21:07:40 -0500

    septimo

--- frontend/src/Cuenta_usuario/Signup/Signup.js
+++ frontend/src/Cuenta_usuario/Signup/Signup.js
@@ -57,7 +57,7 @@ function Signup() {
     setErrors(validationErrors);
     if (Object.keys(validationErrors).length === 0) {
       axios
-        .post(`${electrostar2024-production.up.railway.app}/signup`, values)
+        .post(`${process.env.REACT_APP_BACKEND_URL}/signup`, values)
         .then((res) => {
           const userData = { ...values, email: values.email };
           localStorage.setItem("user", JSON.stringify(userData));
@@ -82,7 +82,7 @@ function Signup() {
       } else {
         // Validate user credentials against backend
         axios
-          .post(`${electrostar2024-production.up.railway.app}/login`, {
+          .post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
             email: email,
             password: password,
           })
